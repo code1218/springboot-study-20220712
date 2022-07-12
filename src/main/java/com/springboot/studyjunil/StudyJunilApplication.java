13beede7b045a0deb8c8ad25f5063fbeaa222fcc
code1218 <skjil1218@gmail.com>
2022-07-12 20:53:10 +0900

    init commit

---
+++ src/main/java/com/springboot/studyjunil/StudyJunilApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjunil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJunilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJunilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyjunil/StudyJunilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjunil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJunilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
