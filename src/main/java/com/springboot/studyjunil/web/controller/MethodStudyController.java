08434ad9a5f38400cf0a710a0c57030d8a39d1ee
code1218 <skjil1218@gmail.com>
2022-07-12 21:52:34 +0900

    포트 설정 및 첫 페이지 로드

---
+++ src/main/java/com/springboot/studyjunil/web/controller/MethodStudyController.java
@@ -0,0 +1,19 @@
+package com.springboot.studyjunil.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MethodStudyController {
+	
+	@GetMapping({"/", "/index"})
+	public String getMethod() {
+		return "method/get";
+	}
+	
+	@GetMapping("/test")
+	public String getMethod2() {
+		return "method/test";
+	}
+	
+}
