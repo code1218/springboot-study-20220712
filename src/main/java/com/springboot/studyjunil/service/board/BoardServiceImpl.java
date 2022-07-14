b6853b71a7115c5a2079baccda76a696bc7bc4aa
code1218 <skjil1218@gmail.com>
2022-07-14 21:52:21 +0900

    게시글 서비스 등록

---
+++ src/main/java/com/springboot/studyjunil/service/board/BoardServiceImpl.java
@@ -0,0 +1,31 @@
+package com.springboot.studyjunil.service.board;
+
+import org.springframework.stereotype.Service;
+
+import com.springboot.studyjunil.web.dto.board.CreateBoardReqDto;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public boolean createBoard(CreateBoardReqDto createBoardReqDto) throws Exception {
+		System.out.println(createBoardReqDto);
+		if(0 == 0) {
+			throw new RuntimeException();			
+		}
+		return true;
+	}
+
+	@Override
+	public boolean updateBoard(int boardcode, CreateBoardReqDto createBoardReqDto) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteBoard(int boardcode) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
--- src/main/java/com/springboot/studyjunil/web/controller/api/board/BoardController.java
+++ src/main/java/com/springboot/studyjunil/web/controller/api/board/BoardController.java
@@ -1,29 +1,34 @@
 package com.springboot.studyjunil.web.controller.api.board;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.studyjunil.service.board.BoardService;
 import com.springboot.studyjunil.web.dto.board.CreateBoardReqDto;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/board")
+@RequiredArgsConstructor
 public class BoardController {
+	
+	private final BoardService boardService;
 
 	//게시글 작성
 	@PostMapping("/content")
 	public ResponseEntity<?> addBoard(CreateBoardReqDto createBoardReqDto){
-		System.out.println("게시글 작성 요청");
-		System.out.println("title: " + createBoardReqDto.getTitle());
-		System.out.println("usercode: " + createBoardReqDto.getUsercode());
-		System.out.println("content: " + createBoardReqDto.getContent());
-		
-		return ResponseEntity.ok().body("test");
-		//return new ResponseEntity<>(title + " 게시글 작성 성공", headers, HttpStatus.BAD_REQUEST);
+		boolean responseData = false;
+		try {
+			responseData = boardService.createBoard(createBoardReqDto);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body(responseData);
+		}
+		return ResponseEntity.ok().body(responseData);
+//		return new ResponseEntity<>(title + " 게시글 작성 성공", headers, HttpStatus.BAD_REQUEST);
 	}
 	
 	//게시글 조회
