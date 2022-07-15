75e332b45bc7b921ce572880d8c928d0630680f6
code1218 <skjil1218@gmail.com>
2022-07-15 21:55:03 +0900

    JOSN 데이터로 요청, DB, Mybatis 연결 board content save 로직

--- src/main/java/com/springboot/studyjunil/service/board/BoardServiceImpl.java
+++ src/main/java/com/springboot/studyjunil/service/board/BoardServiceImpl.java
@@ -2,18 +2,20 @@ package com.springboot.studyjunil.service.board;
 
 import org.springframework.stereotype.Service;
 
+import com.springboot.studyjunil.domain.board.BoardRepository;
 import com.springboot.studyjunil.web.dto.board.CreateBoardReqDto;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
+	
+	private final BoardRepository boardRepository;
 
 	@Override
 	public boolean createBoard(CreateBoardReqDto createBoardReqDto) throws Exception {
-		System.out.println(createBoardReqDto);
-		if(0 == 0) {
-			throw new RuntimeException();			
-		}
-		return true;
+		return boardRepository.save(createBoardReqDto.toEntity()) > 0;
 	}
 
 	@Override
--- src/main/java/com/springboot/studyjunil/web/controller/api/board/BoardController.java
+++ src/main/java/com/springboot/studyjunil/web/controller/api/board/BoardController.java
@@ -2,6 +2,7 @@ package com.springboot.studyjunil.web.controller.api.board;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class BoardController {
 
 	//게시글 작성
 	@PostMapping("/content")
-	public ResponseEntity<?> addBoard(CreateBoardReqDto createBoardReqDto){
+	public ResponseEntity<?> addBoard(@RequestBody CreateBoardReqDto createBoardReqDto){
 		boolean responseData = false;
 		try {
 			responseData = boardService.createBoard(createBoardReqDto);
